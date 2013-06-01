389117415d5b5d8c59365adabe15df278dec5cf6
Erik Mathlener <torongla@xs4all.nl>
2013-06-01 23:57:02 +0200

    Eerste Upload mtbidee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtbidee::Application.config.secret_token = '1cae43b4b99f0f984c058f1240e08626ca7e08c8c274063dbd1e86f72108008a0bc8921f0442ead0cd86f630c9877d20dc8cf662b8d6d613b2a209b2f9c5a8ac'
